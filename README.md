5dc7f88176f740be6ab7ff02a472cc2e08c42fc9
DraqueT <draquemail@gmail.com>
2019-06-18 21:12:09 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OSIntegration_Mac
---
+++ src/osintegration_mac/OSIntegration_Mac.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package osintegration_mac;
+
+import java.awt.Component;
+import java.awt.Image;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+/**
+ *
+ * @author draque
+ */
+public class OSIntegration_Mac {
+
+    public static Object integrateToMac(String displayName, Image icon, Runnable aboutScreen) {
+        System.setProperty("apple.awt.application.name", displayName);
+        
+        com.apple.eawt.Application macApp = com.apple.eawt.Application.getApplication();
+        macApp.setDockIconImage(icon);
+        macApp.setAboutHandler(new com.apple.eawt.AboutHandler() {
+            @Override
+            public void handleAbout(com.apple.eawt.AppEvent.AboutEvent ae) {
+                aboutScreen.run();
+            }
+        });
+        
+        // populate menu bar with placeholders until they can be populated later
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(new JMenu(" "));
+        menuBar.add(new JMenu("  "));
+        menuBar.add(new JMenu("   "));
+        menuBar.add(new JMenu("    "));
+        menuBar.add(new JMenu("     "));
+        macApp.setDefaultMenuBar(menuBar);
+        
+        return menuBar; 
+    }
+    
+    /**
+     * Takes menu bar from PolyGlot and plops it into the Apple bar
+     * @param sourceMenuBar The menu bar this is pulling from (in the main display)
+     * @param targeObject The target menu, which must be instantiated BEFORE nimbus is set
+     * @throws ArrayIndexOutOfBoundsException If there are more menus in PolyGlot than planned 
+     * for here (all have to be created prior to program instantiation)
+     */
+    public static void integrateMacMenuBar(JMenuBar sourceMenuBar, Object targeObject) throws ArrayIndexOutOfBoundsException {
+        if (targeObject instanceof JMenuBar) {
+            JMenuBar targetBar = (JMenuBar)targeObject;
+            com.apple.eawt.Application macApp = com.apple.eawt.Application.getApplication();
+            Component[] sourceComponents = sourceMenuBar.getComponents();
+            Component[] targetComponents = targetBar.getComponents();
+
+            for (int i = 0; i < targetComponents.length; i++) {
+                if (i >= targetComponents.length) {
+                    throw new ArrayIndexOutOfBoundsException("DEVELOPER ERROR: more menu space needed in OSIntegration_Mac module.");
+                } else if (i < sourceComponents.length) {
+                    JMenu targetMenu = (JMenu)targetComponents[i];
+                    JMenu sourceMenu = (JMenu)sourceComponents[i];
+                    
+                    // cycle through each menu and pull its components
+                    for (Component subComp : sourceMenu.getMenuComponents()) {
+                        targetMenu.add(subComp);
+                    }
+                    
+                    targetMenu.setText(sourceMenu.getText());
+                    sourceMenuBar.remove(sourceMenu);
+                } else {
+                    targetComponents[i].setVisible(false);
+                }
+            }
+            
+            sourceMenuBar.setVisible(false); // no need for this once it's in the top apple bar
+            macApp.setDefaultMenuBar(targetBar);
+        }
+    }
+}
